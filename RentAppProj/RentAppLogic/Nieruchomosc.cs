06652f0d74dd375a30ac4ffcee9455ae80865859
Piotr Gretszel <piotr.gretszel@gmail.com>
2017-11-15 17:04:49 +0100

    Zalazek klasy nieruchomosc

---
+++ RentAppProj/RentAppLogic/Nieruchomosc.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentAppLogic
+{
+    class Nieruchomosc
+    {
+        private string ulica;
+        private string miasto;
+        private int nrDomu;
+        private int nrMieszkania;
+        private string kodPocztowy;
+
+        public string Ulica
+        {
+            get
+            {
+                return ulica;
+            }
+
+            set
+            {
+                ulica = value;
+            }
+        }
+
+        public string Miasto
+        {
+            get
+            {
+                return miasto;
+            }
+
+            set
+            {
+                miasto = value;
+            }
+        }
+
+        public int NrDomu
+        {
+            get
+            {
+                return nrDomu;
+            }
+
+            set
+            {
+                nrDomu = value;
+            }
+        }
+
+        public int NrMieszkania
+        {
+            get
+            {
+                return nrMieszkania;
+            }
+
+            set
+            {
+                nrMieszkania = value;
+            }
+        }
+
+        public string KodPocztowy
+        {
+            get
+            {
+                return kodPocztowy;
+            }
+
+            set
+            {
+                kodPocztowy = value;
+            }
+        }
+    }
+}
